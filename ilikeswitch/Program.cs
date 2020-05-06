d3a909037b606767ab9a36f5c4aa95131cc28197
Mincho Park <s2018s28@e-mirim.hs.kr>
2020-05-06 22:13:29 +0900

    학점

--- ilikeswitch/Program.cs
+++ ilikeswitch/Program.cs
@@ -10,6 +10,28 @@ namespace ilikeswitch
     {
         static void Main(string[] args)
         {
+            Console.ReadLine();
+            Console.Write("학년을 입력하세요 : ");
+            var input = Console.ReadLine();
+
+
+            switch (input)
+            {
+                case "level1":
+                    Console.WriteLine("12학점");
+                    break;
+                case "level2":
+                    Console.WriteLine("18학점");
+                    break;
+                case "level3":
+                    Console.WriteLine("10학점");
+                    break;
+                case "level4":
+                    Console.WriteLine("18학점");
+                    break;
+                
+            }
         }
+    
     }
 }
